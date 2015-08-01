e95af2f52149bef1e716bf3b420ba87883d2d77a
Erdin Kacan <unique.ek@gmail.com>
2015-08-01 20:14:26 +0100

    GetAngleBetween added to Vector3Extensions.

--- Assets/Scripts/Extensions/Vector3Extensions.cs
+++ Assets/Scripts/Extensions/Vector3Extensions.cs
@@ -44,4 +44,26 @@ public static class Vector3Extensions
         v.x = (cos * tx) - (sin * ty);
         v.y = (cos * ty) + (sin * tx);
     }
+	
+	 /// <summary>
+	/// 
+	/// </summary>
+	/// <param name="vectorOne">1,0,0 (Right)</param>
+	/// <param name="vectorTwo">0,0,1 (Forward)</param>
+	/// <param name="normal">0,1,0 (Up)</param>
+	/// <returns></returns>
+	public static float GetAngleBetween(this Vector3 vectorOne, Vector3 vectorTwo, Vector3 normal)
+	{
+		// angle in [0,180]
+		float angle = Vector3.Angle(vectorOne, vectorTwo);
+		float sign = Mathf.Sign(Vector3.Dot(normal, Vector3.Cross(vectorOne, vectorTwo)));
+
+		// angle in [-179,180]
+		float signed_angle = angle * sign;
+
+		// angle in [0,360] (not used but included here for completeness)
+		//float angle360 =  (signed_angle + 180) % 360;
+
+		return signed_angle;
+	}
 }
