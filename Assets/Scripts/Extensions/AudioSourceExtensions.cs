3b73324eab2775e0e5844e76033edd950dbb72b3
Erdin Kacan <unique.ek@gmail.com>
2015-08-02 21:52:12 +0100

    AudioSourceExtensions added.

---
+++ Assets/Scripts/Extensions/AudioSourceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+public static class AudioSourceExtensions
+{
+    public static double ConvertSamplesToTime(this AudioSource audioSource,  int samples)
+    {
+        if(audioSource.clip == null) throw new Exception("Audio clip is null.");
+        return samples / (double)audioSource.clip.frequency;
+    }
+
+    public static int ConvertTimeToSamples(this AudioSource audioSource, double time)
+    {
+        if (audioSource.clip == null) throw new Exception("Audio clip is null.");
+        return (int)(time * audioSource.clip.frequency);
+    }
+}
